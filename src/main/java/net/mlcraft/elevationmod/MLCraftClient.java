4daa36b1e1ad8b2090168627aeb8b3edcbbd811c
davidszcz <szczesniakd@ufl.edu>
2024-03-18 20:25:13 -0400

    Initial commit

---
+++ src/main/java/net/mlcraft/elevationmod/MLCraftClient.java
@@ -0,0 +1,10 @@
+package net.mlcraft.elevationmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MLCraftClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
