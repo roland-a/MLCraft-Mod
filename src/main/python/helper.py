d90fc1e2d0fb0753b5edd5bc2251d58c24a03f6a
roland-a <91647122+roland-a@users.noreply.github.com>
2024-07-30 01:16:07 -0400

    Moved multiple functions
    Moved split function to elevation-grabber file
    Moved slice-wrap-around and wrap-around-interpolate function to generate-world file
    
    This was done to remove clutter in the helper file

--- src/main/python/helper.py
+++ src/main/python/helper.py
@@ -145,18 +145,6 @@ def write_with_backup(path, ext, on_path):
     on_path(file)
 
 
-#splits the list into batches, with a batch size at most max_len
-def split(lst: list, max_len: int)->list[list]:
-    bulks = [[]]
-
-    for v in lst:
-        if len(bulks[-1]) >= max_len:
-            bulks.append([])
-
-        bulks[-1].append(v)
-
-    return bulks
-
 # Crops a 2d numpy image to a specified length
 def crop_to_len(img: np.ndarray, final_len: int|tuple[int,int]) -> np.ndarray:
     if isinstance(final_len, int):
@@ -181,48 +169,6 @@ def crop_to_len(img: np.ndarray, final_len: int|tuple[int,int]) -> np.ndarray:
         img = img[:, left_pad_y:-right_pad_y]
 
     return img
-
-
-# Slices a 2d numpy array into a smaller 2d array, while wrapping around if the slice exceeds the boundary
-def slice_wrap_around(arr: np.ndarray, offset: tuple, slice_len: int)->np.ndarray:
-    out = np.empty(shape=(slice_len, slice_len), dtype=arr.dtype)
-
-    for i, _ in np.ndenumerate(out):
-        i = tuple(i%s for i, s in zip(i, arr.shape))
-        i_off = tuple((a+b)%s for a, b, s in zip(i, offset, arr.shape))
-
-        out[i] = arr[i_off]
-
-    return out
-
-# TODO, refactor this function so its more understandable
-def apply_wrap_around(left, right, right_offset: tuple, fn)->np.ndarray:
-    arr = left.copy()
-
-    for i, _ in np.ndenumerate(right):
-        i_off = tuple((i+o) % s for i, o, s in zip(i, right_offset, left.shape))
-
-        arr[i_off] = fn(left[i_off], right[i])
-
-    return arr
-
-
-# Interpolates a 2d numpy array while respecting wraparound boundaries
-def wrap_around_interpolate(arr: np.ndarray, inter_len: int)->np.ndarray:
-    import cv2
-
-    pad_len = 8 * inter_len
-
-    # Adds wrap-around padding to effectively allow wraparound interpolation to the non-padded areas
-    arr = np.pad(arr, (pad_len, pad_len), mode='wrap')
-
-    arr = cv2.resize(arr, (arr.shape[0] * inter_len, arr.shape[1] * inter_len), interpolation=cv2.INTER_LANCZOS4)
-
-    # Removes the padding
-    # TODO, use crop_to_len to remove padding
-    arr = arr[pad_len * inter_len:-pad_len * inter_len, pad_len * inter_len:-pad_len * inter_len]
-
-    return arr
 
 
 # Converts a 2d numpy array to a greyscale png
